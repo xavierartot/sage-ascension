fbc61452f8f306f90a07079965631a1d03745031
xavierartot <artotal@gmail.com>
2017-02-09 11:47:44 -0600

    add starter variables

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,21 @@
-<nav class="navbar navbar-light bg-faded">
-  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-  <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    &#9776;
-  </button>
-  <div class="collapse navbar-toggleable-sm" id="navbarCollapse">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
+<?php
+  // For use with Sagextras (https://github.com/storm2k/sagextras)
+?>
+
+<header class="" role="banner">
+  <div class="container-fluid">
+    <nav class="navbar navbar-light bg-faded">
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <button class="navbar-toggler pull-right hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      &#9776;
+      </button>
+      <div class="collapse navbar-toggleable-sm" id="navbarCollapse">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'pull-md-right nav navbar-nav']);
-      endif;
-      ?>
+        endif;
+        ?>
+      </div>
+    </nav>
   </div>
-</nav>
+</header>
